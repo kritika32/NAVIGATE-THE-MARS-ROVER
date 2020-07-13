d5042ef4c6e65b926c570abd93a5ae31342cbd16
kritika32 <kritikayadav950@gmail.com>
2020-07-13 23:52:13 +0530

    Bfs path+walls applied

---
+++ backend/routes/public2/modules/createMatrix.js
@@ -0,0 +1,23 @@
+function createMatrix() {
+  function initArray(matrix, rows, cols) {
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < cols; j++) {
+        if (i == 0 || j == 0 || i == rows - 1 || j == cols - 1) {
+          matrix[i][j] = 5;
+        } else {
+          matrix[i][j] = 0;
+        }
+      }
+    }
+  }
+  var matrix = new Array(rows);
+  for (let i = 0; i < rows; i++) {
+    matrix[i] = new Array(cols);
+  }
+
+  initArray(matrix, rows, cols);
+  return matrix;
+}
+console.log("CreateMatrix.js");
+var matrix = createMatrix();
+export default matrix;
